a6108198db013aa3f3298c21f6c56305b8818f53
Katy Cassidy <ihatetoast@gmail.com>
2026-06-29 20:44:40 -0500

    adding as many weak / n declension nouns for the last game

--- src/types.ts
+++ src/types.ts
@@ -1,46 +1,46 @@
 export interface Sentence {
   de: string;
   en: string;
-  case: 'nom' | 'acc' | 'dat' | 'gen' | '';
+  case: "nom" | "acc" | "dat" | "gen" | "";
 }
-export type ArticleType = 'der' | 'die' | 'das';
+export type ArticleType = "der" | "die" | "das";
 
 export const THEMES = [
-  'airport',
-  'animals',
-  'art', // eventually add music, studio art, theater and
-  'body',
-  'buildings',
-  'city', // if mostly in a city, then city. 
-  'clothing',
-  'country', // rural farm, not geo political. if mostly rural country, then here and not city
-  'doctor',
-  'family', // can include pets, marriage, divorce
-  'finance',
-  'food',
-  'home',
-  'hospital',
-  'hotel',
-  'nature',
-  'office',
-  'people',
-  'profession',
-  'personal', // covers grooming, things in one's desk or private things
-  'religion',
-  'restaurant',
-  'school',
-  'science', // eventually add biology, geology, zoology
-  'shopping',
-  'social',
-  'sport',
-  'time',
-  'transportation',
-  'travel',
-  'vehicles',
-  'weather',
-  'work',
-  'zoo', // exotics. no dog cat. also name for zoo, and also zoo keeper
-  'misc', // remove when a theme is assigned
+  "airport",
+  "animals",
+  "art", // eventually add music, studio art, theater and
+  "body",
+  "buildings",
+  "city", // if mostly in a city, then city.
+  "clothing",
+  "country", // rural farm, not geo political. if mostly rural country, then here and not city
+  "doctor",
+  "education",
+  "family", // can include pets, marriage, divorce
+  "finance",
+  "food",
+  "home",
+  "hospital",
+  "hotel",
+  "nature",
+  "office",
+  "people",
+  "profession",
+  "personal", // covers grooming, things in one's desk or private things
+  "religion",
+  "restaurant",
+  "science", // eventually add biology, geology, zoology
+  "shopping",
+  "social",
+  "sport",
+  "time",
+  "transportation",
+  "travel",
+  "vehicles",
+  "weather",
+  "work",
+  "zoo", // exotics. no dog cat. also name for zoo, and also zoo keeper
+  "misc", // remove when a theme is assigned
 ] as const;
 
 export type Theme = (typeof THEMES)[number];
@@ -74,25 +74,25 @@ type NotesType = {
   genitiveNote?: string;
 };
 
-export type ModeProp = 'practice' | 'test' | 'home';
+export type ModeProp = "practice" | "test" | "home";
 // controlling decksize until I have >500 completed: true. egads
 export type DeckSize = 5 | 10 | 20 | 25 | 50;
 
-export type MiniTestType = 'eng-ger-mini' | 'ger-eng-mini';
+export type MiniTestType = "eng-ger-mini" | "ger-eng-mini";
 export type TestType =
-  | 'article'
-  | 'plural'
-  | 'german-full'
+  | "article"
+  | "plural"
+  | "german-full"
   | MiniTestType
-  | 'weak-masc';
+  | "weak-masc";
 
-export type AnswerState = 'waiting' | 'skipped' | 'correct' | 'incorrect';
+export type AnswerState = "waiting" | "skipped" | "correct" | "incorrect";
 
-export type GameState = 'waiting' | 'active' | 'over';
+export type GameState = "waiting" | "active" | "over";
 
-export type HintState = null | 'scrambled' | 'revealed';
+export type HintState = null | "scrambled" | "revealed";
 
-export type WeakMascAnswer = 'yes' | 'no' | 'not-masc';
+export type WeakMascAnswer = "yes" | "no" | "not-masc";
 
 export type GameProps = {
   words: VocabEntry[];
